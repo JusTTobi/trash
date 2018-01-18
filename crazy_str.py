bbf7ab43f7d0121dbdf99afa068f9b8a1d5b71d5
alashkov83 <bjabpbr>
2018-01-18 22:10:31 +0300

    Add lineanddots

--- crazy_str.py
+++ crazy_str.py
@@ -3,6 +3,7 @@ from functools import cmp_to_key
 
 list_1 = ['1123aaaare21\n', 'AAAAA11213ZZZ\n', '232fdfD321\n', 'DFFFGG12', '21sdsss1\n']
 
+
 def func(a, b):
     if a[1] or b[1]:
         return 0
@@ -13,8 +14,8 @@ def func(a, b):
     elif a[2] == b[2]:
         return 0
 
-print(sorted([(''.join(j for j in line if j.isalpha())+'\n', ''.join(j for j in line if j.isalpha()
-        ).isupper(), sum(map(int, ''.join(s if s.isdigit() else ' ' for s in line
-        ).split()))) for line in list_1], key=cmp_to_key(func)))
 
-
+print(sorted([(''.join(j for j in line if j.isalpha()) + '\n', ''.join(
+    j for j in line if j.isalpha()).isupper(), sum(map(
+    int, ''.join(s if s.isdigit() else ' ' for s in line
+    ).split()))) for line in list_1], key=cmp_to_key(func)))
