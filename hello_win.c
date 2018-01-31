ddbc93d08f7908e154bfc06dcb26dd50b35f90f1
alashkov83 <alashkov83@gmail.com>
2018-01-31 12:40:07 +0300

    Create hello_win.c

---
+++ hello_win.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <windows.h>
+
+typedef BOOL (WINAPI *SETCONSOLEFONT)(HANDLE, DWORD);
+SETCONSOLEFONT SetConsoleFont;
+
+void main(){
+  HMODULE hmod = GetModuleHandleA("KERNEL32.DLL");
+  SetConsoleFont =(SETCONSOLEFONT) GetProcAddress(hmod, "SetConsoleFont");
+  SetConsoleFont(GetStdHandle(STD_OUTPUT_HANDLE), 8);
+  SetConsoleCP(1251);
+  SetConsoleOutputCP(1251);
+  printf("Привет Мир!\nВот такая фигня нужна, чтобы вывести текст на русском языке в консоле Wndows :-)\n");
+  getchar();
+}
