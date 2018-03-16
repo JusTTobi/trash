4e712d20fa02699ba044b04f16b2771ae6c8b486
svm17u568 <vlad.suhanov.1998@gmail.com>
2018-03-16 12:46:49 +0300

    Add decor

---
+++ decor.py
@@ -0,0 +1,20 @@
+import time
+def my_decorator(func):
+    def wrap(*a, **b):
+        print('Функция ' + func.__name__ + ' начала работу!')
+        t=time.clock()
+        res = func(*a, **b)
+        print('Функция ' + func.__name__ + ' закончила работу через ' +str(time.clock() - t))
+        return res
+    return wrap
+
+@my_decorator
+def progress2(start, n, step):
+    i = 1
+    while i < n:
+        start+=step
+        i+=1
+    return start
+
+print(progress2(2, 1000, 2))
+
